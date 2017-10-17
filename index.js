89090636362ea67074bcd37febfd4388c4854869
Harendra Singh <66hari@gmail.com>
2017-10-18 00:25:08 +0530

    Using google api

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config()
 var express = require('express');
+var googleApi = require('./lib/googleApi');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,24 +12,10 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index', {
-    videos: [
-      {
-        "url": "https://www.youtube.com/embed/I_ypN8iKuwk",
-        "image": "https://img.youtube.com/vi/I_ypN8iKuwk/0.jpg",
-        "title": "BIGG BOSS BIGGEST FIGHTS ROAST"
-      },
-      {
-        "url": "https://www.youtube.com/embed/ruHMqQJ6PNE",
-        "image": "https://img.youtube.com/vi/ruHMqQJ6PNE/0.jpg",
-        "title": "Truth or Dare, Husband vs. Wife feat. Permanent Roommates"
-      },
-      {
-        "url": "https://www.youtube.com/embed/yJD1Iwy5lUY",
-        "image": "https://img.youtube.com/vi/yJD1Iwy5lUY/0.jpg",
-        "title": "If Google Was A Guy (Part 3)"
-      }
-    ]
+  googleApi.getToken((err, data) => {
+      console.log({err, data})
+      data = data || {videos: []};
+      return response.render('pages/index', data);
   });
 });
 
