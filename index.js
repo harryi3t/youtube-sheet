864b175d36a0ccfdbeb883fcf9b9d9b3ecef34c5
Harendra Singh <66hari@gmail.com>
2017-10-19 03:01:05 +0530

    Adds email input field, gets tracked in a separate worksheet

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 var express = require('express'),
     async = require('async'),
+    bodyParser = require('body-parser'),
     _ = require('lodash'),
     GoogleSpreadsheet = require('google-spreadsheet'),
 
@@ -15,6 +16,8 @@ var express = require('express'),
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -46,6 +49,26 @@ app.get('/', function (request, response) {
         return response.render('pages/index', data);
     });
 });
+
+app.post('/email', function (request, response) {
+    var email = request.body.email;
+    // @todo pass success/failure message to redirect
+
+    spreadsheet.getRows(2, {}, (err, rows) => {
+        if (err) {
+            console.log('error', err);
+            return response.redirect('/');
+        }
+
+        spreadsheet.addRow(2, {
+            's.no': rows.length + 1,
+            'email': email,
+            'date': new Date()
+        }, function () {
+            return response.redirect('/');
+        });
+    });
+})
 
 app.listen(app.get('port'), function () {
     console.log('Node app is running on port', app.get('port'));
